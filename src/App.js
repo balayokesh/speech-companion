9d5dae694a0a7a9c2ef003c088f5b5a011950560
balayokesh <balayokeshmani@gmail.com>
2021-11-13 20:50:01 +0530

    Implement basic UI, texttoSpeech functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './Styles/App.css';
+import './Styles/App.css'; 
+
+import MicrophoneMuted from './Images/microphone-muted.svg';
+import SpeakerMuted from './Images/speaker-muted.svg';
 
 function App() {
+
+  const speakContent = () => {
+    var msg = new SpeechSynthesisUtterance();
+    msg.text = document.getElementById('textarea').innerHTML;
+    document.getElementById('speaker').src = './images/speaker.svg';
+    window.speechSynthesis.speak(msg);
+  }
+
   return (
-    <div>
-      Hello, World!
+    <div id='flex-parent'>
+        <img src={MicrophoneMuted} id='microphone' alt='microphone' className='flex-element'/>
+        <div contentEditable id='textarea' className='flex-element'>
+          Click the mike button and start speaking
+          (or)
+          Click on right speaker button to speak the contents in the box
+        </div>
+        <img src={SpeakerMuted} onClick={speakContent} id='speaker' alt='speaker' className='flex-element'/>
     </div>
   );
 }
