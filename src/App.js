6316539d1d3439e0714a2966269d657596b48f5c
adityasimant <adityasimant88@gmail.com>
2023-01-14 21:13:16 +0530

    Added CONTRIBUTING.md file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import style sheets
-import './Styles/App.css'; 
+import './Styles/App.css';
 
 // Import NPM packages
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
@@ -21,64 +21,62 @@ function App() {
     var msg = new SpeechSynthesisUtterance();
     msg.text = document.getElementById('textarea').textContent;
     window.speechSynthesis.speak(msg);
-    
+
     msg.addEventListener("end", () => {
       document.getElementById("speaker").src = SpeakerMuted;
     });
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     document.addEventListener('keydown', detectKeyDown, true)
   }, [])
 
-  const detectKeyDown = (e) =>{
+  const detectKeyDown = (e) => {
     e.preventDefault()
-    if(e.key === "s" &&  e.ctrlKey){
-        speakContent()
+    if (e.key === "s" && e.ctrlKey) {
+      speakContent()
     }
-    if(e.key === "m" &&  e.ctrlKey){
+    if (e.key === "m" && e.ctrlKey) {
 
-      if(listening){
+      if (listening) {
         SpeechRecognition.stopListening()
       }
-      else{
+      else {
         SpeechRecognition.startListening()
-      }
-
+      } 
     }
-
   }
 
   const {
     transcript,
     listening,
     browserSupportsSpeechRecognition
-  } = useSpeechRecognition();  
+  } = useSpeechRecognition();
 
   const reset = () => {
     window.location.reload();
   }
-    
+
   if (!browserSupportsSpeechRecognition) {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
-  
+
 
 
   return (
     <div id='flex-parent'>
-        <img src={listening ? Microphone : MicrophoneMuted} id='microphone' alt='microphone' className='flex-element' onClick={listening ? SpeechRecognition.stopListening : SpeechRecognition.startListening }/>
-        <div className='flex-element' id='center'>
-          <div contentEditable id='textarea'>
-            {transcript ? <p>{transcript}</p> : <p>Click the mike button and start speaking
+      <img src={listening ? Microphone : MicrophoneMuted} id='microphone' alt='microphone' className='flex-element' onClick={listening ? SpeechRecognition.stopListening : SpeechRecognition.startListening} />
+      <div className='flex-element' id='center'>
+        <div contentEditable id='textarea'>
+          {transcript ? <p>{transcript}</p> : <p>Click the mike button and start speaking
             (or)
             Click on right speaker button to speak the contents in the box</p>}
-          </div>
-          <button onClick={reset} id='reset'>Reset</button>
         </div>
-        
-        <img src={SpeakerMuted} onClick={speakContent} id='speaker' alt='speaker' className='flex-element'/>
+        <button onClick={reset} id='reset'>Reset</button>
+      </div>
+
+      <img src={SpeakerMuted} onClick={speakContent} id='speaker' alt='speaker' className='flex-element' />
     </div>
   );
 }
